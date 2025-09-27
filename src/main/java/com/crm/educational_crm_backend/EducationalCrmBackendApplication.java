6876f7285922db1575d683d9ca26275d029835a6
Gopichand Bantuboyina <bantuboyinagopichand@gmail.com>
2025-09-27 18:28:50 +0530

    Initial commit of Spring Boot backend

---
+++ src/main/java/com/crm/educational_crm_backend/EducationalCrmBackendApplication.java
@@ -0,0 +1,13 @@
+package com.crm.educational_crm_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalCrmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationalCrmBackendApplication.class, args);
+	}
+
+}
