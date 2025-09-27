e6b36d2c051407f36298910755d9e89b85a33b69
Gopichand Bantuboyina <bantuboyinagopichand@gmail.com>
2025-09-27 22:45:54 +0530

    Connected Spring Boot with PostgreSQL and tested DB connection

--- src/main/java/com/crm/educational_crm_backend/EducationalCrmBackendApplication.java
+++ src/main/java/com/crm/educational_crm_backend/EducationalCrmBackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EducationalCrmBackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EducationalCrmBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EducationalCrmBackendApplication.class, args);
+    }
 }
